99e8a7c8fdb882eff498f3404638a117c9794d2d
sbarrow825 <66639249+sbarrow825@users.noreply.github.com>
2020-07-03 11:40:34 -0700

    preparing to move map functionality to Sergio's results page

--- Sam.js
+++ Sam.js
@@ -9,34 +9,16 @@ $(document).ready(function () {
     //     });
     // }
 
-    // initMap();
 
-    // function getZillowInfo() {
-
-    //     var cityName = $(this).text();
-    //     var address = "9241 13th Ave SW"
-    //     var proxy = "https://cors-anywhere.herokuapp.com/";
-    //     var queryURL = proxy + "https://www.zillow.com/webservice/GetSearchResults.htm?zws-id=X1-ZWz1hvnpswvt3f_39zrb&address=" + address + "&citystatezip=" + cityName;
-
-
-    //     $.ajax({
-    //         type: "GET",
-    //         url: queryURL
-    //       }).then((res) => {
-    //         console.log(res);
-    //       });
-
-    // }
-
-    var lat = 37.773972;
-    var lng = -122.431297;
+    var lat = 48.962940;
+    var lng = -122.425080;
 
     var mapOptions = {
         zoom: 8,
         mapTypeId: 'roadmap',
         center: {
-            lat: 37.773972,
-            lng: -122.431297
+            lat: lat,
+            lng: lng
         }
     };
 
@@ -49,4 +31,30 @@ $(document).ready(function () {
         },
         map: map
     });
+    
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://realtymole-rental-estimate-v1.p.rapidapi.com/rentalPrice?compCount=10&longitude=" + lng + "&latitude=" + lat,
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "realtymole-rental-estimate-v1.p.rapidapi.com",
+            "x-rapidapi-key": "e6fb33abd8msh28d4bbf4c53771cp14f30ajsn27f9f4b14f17"
+        }
+    }
+    
+    $.ajax(settings).done(function (response) {
+        for (i = 0; i <= response.listings.length - 1; i += 1) {
+            var newLat = response.listings[i].latitude;
+            var newLng = response.listings[i].longitude;
+            new google.maps.Marker({
+                label: response.listings[i].address,
+                position: {
+                    lng: newLng,
+                    lat: newLat
+                },
+                map: map
+            });
+        }
+    })
 });
