f19352fc1d14c49b4ea6d4284efa30beb2182aed
sbarrow825 <66639249+sbarrow825@users.noreply.github.com>
2020-07-06 08:34:45 -0700

    pushing finished script.js with leaflet map working

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var lng;
+var lat;
 var workAddress;
 var key;
 var queryURL;
@@ -9,6 +10,9 @@ var listingAddress;
 var listingPrice;
 var commuteTimeQueryURL;
 var commuteTime;
+var listingAddressArray = [];
+var listingPriceArray = [];
+var commuteTimeArray = [];
 
 var searchTerm = document.querySelector("#srch-trm");
 var searchButton = document.querySelector("#find-address");
@@ -16,14 +20,12 @@ var homeButton = document.querySelector("#home-button");
 var buttonPage = document.querySelector(".button-page");
 var searchPage = document.querySelector(".search-page");
 
-let address;
 
 searchButton.addEventListener("click", function (event) {
   event.preventDefault();
   buttonPage.style.display = "none";
   searchPage.setAttribute("style", "display: block");
-  console.log("hello")
-  getSearchReults();
+  getSearchResults();
 });
 
 homeButton.addEventListener("click", function (event) {
@@ -33,103 +35,120 @@ homeButton.addEventListener("click", function (event) {
 });
 
 
-function getSearchReults() {
+
+function getSearchResults() {
+  var listingAddressArray = [];
+  var listingPriceArray = [];
+  var commuteTimeArray = [];
+  $("#response-cards").empty();
   var workAddress = $("#srch-trm").val();
-  console.log(workAddress);
   var key = "AIzaSyCi3dzreMrxNzmaHrULhs_SCJRMpzt8FbE";
   var queryURL = "https://maps.googleapis.com/maps/api/geocode/json?address=" + workAddress + "&key=AIzaSyCi3dzreMrxNzmaHrULhs_SCJRMpzt8FbE"
+  // Converting the searched address (workAddress) into lat lng and creating a marker on the map
   $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function (response) {
-    console.log(response);
     var lat = response.results[0].geometry.location.lat;
     var lng = response.results[0].geometry.location.lng;
-
-    // var mapOptions = {
-    //   zoom: 15,
-    //   mapTypeId: 'roadmap',
-    //   center: {
-    //     lat: lat,
-    //     lng: lng
-    //   }
-    // };
-
-    var map = L.map('map', {
-      center: [51.505, -0.09],
-      zoom: 13
-  });
-
-    // new google.maps.Marker({
-    //   label: "Searched Address",
-    //   position: {
-    //     lat: lat,
-    //     lng: lng,
-    //   },
-    //   map: map
-    // })
-
-    var settings = {
-      "async": true,
-      "crossDomain": true,
-      "url": "https://realtymole-rental-estimate-v1.p.rapidapi.com/rentalPrice?compCount=10&longitude=" + lng + "&latitude=" + lat,
-      "method": "GET",
-      "headers": {
+    var realtyData = $.parseJSON($.ajax({
+      async: true,
+      crossDomain: true,
+      url: "https://realtymole-rental-estimate-v1.p.rapidapi.com/rentalPrice?compCount=10&longitude=" + lng + "&latitude=" + lat,
+      method: "GET",
+      headers: {
         "x-rapidapi-host": "realtymole-rental-estimate-v1.p.rapidapi.com",
         "x-rapidapi-key": "e6fb33abd8msh28d4bbf4c53771cp14f30ajsn27f9f4b14f17"
-      }
+      },
+      dataType: "json",
+      async: false
+    }).responseText); // This will wait until you get a response from the ajax request.
+
+    for (i = 0; i <= realtyData.listings.length - 1; i += 1) {
+      listingAddress = realtyData.listings[i].address;
+      listingPrice = realtyData.listings[i].price;
+      listingAddressArray.push(listingAddress);
+      listingPriceArray.push(listingPrice);
+      console.log(listingAddressArray);
+      console.log(listingPriceArray);
     }
 
-    $.ajax(settings).then(function (response1) {
-      for (i = 0; i <= response1.listings.length - 1; i += 1) {
-        var newLat = response1.listings[i].latitude;
-        var newLng = response1.listings[i].longitude;
-        // new google.maps.Marker({
-        //   label: response1.listings[i].address,
-        //   position: {
-        //     lng: newLng,
-        //     lat: newLat
-        //   },
-        //   map: map
-        // })
-        var listingAddress = response1.listings[i].formattedAddress;
-        var listingPrice = response1.listings[i].price;
-        var commuteTimeQueryURL = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=" + listingAddress + "&destinations=" + workAddress + "&key=AIzaSyCi3dzreMrxNzmaHrULhs_SCJRMpzt8FbE"
-        $.ajax({
-          url: commuteTimeQueryURL,
-          method: "GET",
-        }).then(function (response) {
-          var commuteTime = response.rows[0].elements[0].duration.text;
-          var divCard = $("<div>");
-          divCard.attr("class", "card m-2");
-          var imagTag = $("<img>");
-          var linkTag = $("<a>");
-          //Address of the link 
-          linkTag.attr("href", "#");
-          linkTag.attr("class", "btn-warning")
-          // address of the image to show 
-          imagTag.attr("src", "#");
-          imagTag.attr("class", "card-img-top");
-          // insert picture name from response 
-          imagTag.attr("atl", "picture");
-          linkTag.append(imagTag);
-          divCard.append(linkTag);
-          var divCardBody = $("<div>");
-          divCardBody.attr("class", "card-body");
-          var h5text = $("<h5>").text(listingAddress);
-          h5text.attr("class", "card-title");
-          var ptext = $("<p>").text("Price: $" + listingPrice);
-          ptext.attr("class", "card-text");
-          divCardBody.append(h5text);
-          divCardBody.append(ptext);
-          var ptext2 = $("<p>").text("Commute time: " + commuteTime);
-          ptext2.attr("class", "card-text");
-          divCardBody.append(ptext2);
-          divCard.append(divCardBody);
-          //Appending to the div card
-          $("#response-cards").append(divCard);
-        })
-      }
-    })
-  });
+    for (i = 0; i <= listingAddressArray.length - 1; i += 1) {
+
+      var divCard = $("<div>");
+      divCard.attr("class", "card m-2");
+      var imagTag = $("<img>");
+      var linkTag = $("<a>");
+      //Address of the link 
+      linkTag.attr("class", "btn-warning")
+      // address of the image to show 
+      imagTag.attr("class", "card-img-top");
+      // insert picture name from response 
+      imagTag.attr("atl", "picture");
+      linkTag.append(imagTag);
+      divCard.append(linkTag);
+      var divCardBody = $("<div>");
+      divCardBody.attr("class", "card-body-listing");
+      divCardBody.addClass("card-body")
+      var h5text = $("<h5>").text(listingAddressArray[i]);
+      h5text.attr("class", "card-title");
+      var ptext = $("<p>").text("Price: $" + listingPriceArray[i]);
+      ptext.attr("class", "card-text");
+      divCardBody.append(h5text);
+      divCardBody.append(ptext);
+      divCard.append(divCardBody);
+      //Appending to the div card
+      $("#response-cards").append(divCard);
+    }
+
+
+    var service = new google.maps.DistanceMatrixService();
+    var origins = listingAddressArray;
+    console.log(origins);
+    var destination = workAddress;
+    service.getDistanceMatrix(
+      {
+        origins: origins,
+        destinations: [destination],
+        travelMode: 'DRIVING',
+      }, callback)
+
+    function callback(response) {
+      $(".card-body-listing").each(function (index) {
+        var ptext2 = $("<p>").text("Commute time: " + response.rows[index].elements[0].duration.text);
+        ptext2.attr("class", "card-text");
+        $(this).append(ptext2);
+      })
+    }
+
+    console.log(lat);
+    console.log(lng);
+    var mymap = L.map('mapid').setView([lat, lng], 20);
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic2JhcnJvdzgyNSIsImEiOiJja2NhbXd5ZzUxd3IzMnJtcmJ4N2owZmtsIn0.vEUrS18G5gRKMxTVJ0V34A', {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: 'mapbox/streets-v11',
+      tileSize: 512,
+      zoomOffset: -1,
+      accessToken: 'your.mapbox.access.token'
+    }).addTo(mymap);
+
+    var marker = L.marker([lat, lng]).addTo(mymap);
+    marker.bindPopup("<b>Searched Address</b>").openPopup();
+
+    for (i = 0; i <= listingAddressArray.length; i += 1) {
+      console.log(listingAddressArray[i])
+      var queryURL = "https://maps.googleapis.com/maps/api/geocode/json?address=" + listingAddressArray[i] + "&key=AIzaSyCi3dzreMrxNzmaHrULhs_SCJRMpzt8FbE"
+      // Converting each listing address into lat lng and creating a marker on the map
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function (response) {
+        var lat = response.results[0].geometry.location.lat;
+        var lng = response.results[0].geometry.location.lng;
+        var marker = L.marker([lat, lng]).addTo(mymap);
+      })
+    }
+
+  })
 }
